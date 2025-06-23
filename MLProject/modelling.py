8e1bf3e527447429bdf17f520a589f0ef52d6521
AbuKhalid86 <surv.massenrempulu>
2025-06-23 20:04:24 +0800

    edit modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,21 +1,24 @@
+import argparse
 import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 
-# Aktifkan autolog agar semua param, metrik, model tercatat otomatis
-mlflow.sklearn.autolog()
+# Argument untuk folder data
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", type=str, default="dataset_preprocessing")
+args = parser.parse_args()
 
-# Baca data hasil preprocessing dari folder lokal
-train = pd.read_csv("train_data_scaled.csv")
-test = pd.read_csv("test_data_scaled.csv")
+# Baca data
+train = pd.read_csv(f"{args.data_path}/train_data_scaled.csv")
+test = pd.read_csv(f"{args.data_path}/test_data_scaled.csv")
 
-# Pisah fitur dan target
 X_train, y_train = train.drop(columns="target"), train["target"]
 X_test, y_test = test.drop(columns="target"), test["target"]
 
-# Mulai run experiment lokal (Tracking URI default: localhost)
-with mlflow.start_run(run_name="basic_autolog_rf"):
+# Autolog MLflow
+mlflow.sklearn.autolog()
+
+with mlflow.start_run(run_name="autolog_random_forest"):
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
-    # Tidak perlu log_model lagi, karena autolog sudah mencatat otomatis
